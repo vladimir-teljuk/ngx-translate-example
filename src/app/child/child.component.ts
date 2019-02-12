173b4fbe9cc8fea6bda0cadf55b678df37aaa673
vladimir_teliuk <vladimir.teljuk@gmail.com>
2019-02-12 16:41:26 +0200

    new component

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {TranslateService} from '@ngx-translate/core';
 
 @Component({
-  selector: 'app-child',
-  templateUrl: './child.component.html',
-  styleUrls: ['./child.component.scss']
+    selector: 'app-child',
+    templateUrl: './child.component.html',
+    styleUrls: ['./child.component.scss']
 })
 export class ChildComponent implements OnInit {
 
-  constructor(private translate: TranslateService) { }
+    public exampleArray = ['item_one, item_two, item_three, item_four'];
 
-  ngOnInit() {
-  }
+    constructor(private translate: TranslateService) {
+    }
+    ngOnInit() {
+        this.translate.get('item_one').subscribe(res => console.log(res));
+    }
 
 }
